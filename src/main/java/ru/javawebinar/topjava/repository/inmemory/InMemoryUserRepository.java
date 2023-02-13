a2a810be35b3ecfa319b6414f2465801f8f2f3e7
Vasil9202 <118492858+Vasil9202@users.noreply.github.com>
2023-02-13 04:32:07 +0300

    Шаг 1.Имплементировать InMemoryUserRepository по аналогии с InMemoryMealRepository

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
@@ -3,43 +3,75 @@ package ru.javawebinar.topjava.repository.inmemory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 @Repository
 public class InMemoryUserRepository implements UserRepository {
     private static final Logger log = LoggerFactory.getLogger(InMemoryUserRepository.class);
 
+    private final Map<Integer, User> repository = new ConcurrentHashMap<>();
+    private final AtomicInteger counter = new AtomicInteger(0);
+
+
+
     @Override
     public boolean delete(int id) {
         log.info("delete {}", id);
-        return true;
+        return repository.remove(id) != null;
     }
 
     @Override
     public User save(User user) {
         log.info("save {}", user);
-        return user;
+        if(user.isNew()){
+        repository.put(counter.incrementAndGet(),user);
+        return user;}
+        else{
+        return repository.computeIfPresent(user.getId(), (id, oldUser) -> {
+            oldUser.setName(user.getName());
+            oldUser.setEmail(user.getEmail());
+            oldUser.setPassword(user.getPassword());
+            oldUser.setRegistered(user.getRegistered());
+            oldUser.setCaloriesPerDay(user.getCaloriesPerDay());
+            oldUser.setEnabled(user.isEnabled());
+            oldUser.setRoles(user.getRoles());
+            return oldUser;
+        });
+    }
     }
 
     @Override
     public User get(int id) {
         log.info("get {}", id);
-        return null;
+        return repository.get(id);
     }
 
     @Override
     public List<User> getAll() {
         log.info("getAll");
-        return Collections.emptyList();
+
+        return repository.entrySet().stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.comparing(User::getName)))
+                .map(Map.Entry::getValue)
+                .collect(Collectors.toList());
     }
 
     @Override
     public User getByEmail(String email) {
         log.info("getByEmail {}", email);
-        return null;
+        return repository.values().stream()
+                .filter(u -> u.getEmail().equals(email))
+                .findFirst()
+                .orElse(null);
     }
 }
