914995c948b7deed44aa9ef91310b8b0e3bd65d5
Vasil9202 <118492858+Vasil9202@users.noreply.github.com>
2023-03-27 13:27:52 +0300

    7_0_ignore_test.patch

--- src/test/java/ru/javawebinar/topjava/service/AbstractUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractUserServiceTest.java
@@ -1,16 +1,22 @@
 package ru.javawebinar.topjava.service;
 
+import org.junit.Assume;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.CacheManager;
+import org.springframework.core.env.Environment;
 import org.springframework.dao.DataAccessException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 import ru.javawebinar.topjava.UserTestData;
 import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
 
 import javax.validation.ConstraintViolationException;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.Set;
@@ -18,6 +24,7 @@ import ru.javawebinar.topjava.repository.JpaUtil;
 
 import static org.junit.Assert.assertThrows;
 import static ru.javawebinar.topjava.UserTestData.*;
+
 
 public abstract class AbstractUserServiceTest extends AbstractServiceTest {
 
@@ -28,12 +35,19 @@ public abstract class AbstractUserServiceTest extends AbstractServiceTest {
     private CacheManager cacheManager;
 
     @Autowired
+    private Environment environment;
+    @Autowired
     protected JpaUtil jpaUtil;
 
     @Before
     public void setup() {
         cacheManager.getCache("users").clear();
         jpaUtil.clear2ndLevelHibernateCache();
+        Assume.assumeTrue(isJdbcEnvironment());
+    }
+
+    private boolean isJdbcEnvironment() {
+        return !Arrays.asList(environment.getActiveProfiles()).contains("jdbc");
     }
 
     @Test
